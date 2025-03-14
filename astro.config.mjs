a64f9579b50b91aac34046262daa5e4fd1b3028c
Lionlifei <473857331@qq.com>
2025-03-14 11:34:47 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Lionlifei.github.io',
-  base: '/',
+  site: 'https://Lionlifei.github.io/',
   integrations: [tailwind()]
 });
